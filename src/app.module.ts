eba479a82258fedc5d47111f2aa571b4f45230fc
Rafael Cena <rafael.cena@hotmail.com>
2022-06-16 11:02:19 -0300

    implementando entidade categorias part1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
@@ -9,7 +10,9 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRoot(process.env.DATABASE_USER,
     { useNewUrlParser: true, useUnifiedTopology: true}),
 
-    JogadoresModule],
+    JogadoresModule,
+
+    CategoriasModule],
   controllers: [],
   providers: [],
 })
